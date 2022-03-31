1da22cf1f4ba7bb26b8a802e15b64bbde2e56117
Pepe Fernández <jfernandezmarin0@uoc.edu>
2022-04-01 00:16:01 +0200

    Adds Firebase

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDb2ver9X-LTcTdsa84J-cTHDOe16yh82A",
+  authDomain: "expert-garden.firebaseapp.com",
+  projectId: "expert-garden",
+  storageBucket: "expert-garden.appspot.com",
+  messagingSenderId: "644824745090",
+  appId: "1:644824745090:web:fe962dbbaf41c4a4e4e09b",
+  measurementId: "G-HV58GWH706"
+};
+
+//TODO TERMINAR DE CONFIGURAR ANALYTICS EN APPS https://docs.expo.dev/guides/setup-native-firebase/
+initializeApp(firebaseConfig);
+
+
 export default function App() {
   return (
     <View style={styles.container}>
