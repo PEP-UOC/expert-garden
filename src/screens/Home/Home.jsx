f6a9f2deb5acb03f2844e421a6b43c01c276495f
Pepe Fernández <jfernandezmarin0@uoc.edu>
2022-05-19 19:54:54 +0200

    Deactivates default debug

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -76,7 +76,7 @@ export const HomeScreen = ({ debug, navigation }) => {
 							<View style={{ ...gloStyles.view }}>
 
 								<View style={{ ...gloStyles.section.primary }}>
-									<TitleScreen icon={''} exterStyles={{ wrapper: { marginBottom: 30 }, primaryText: { fontSize: 36 }, secondaryText: { fontSize: 36 } }} primaryText={user?.additionalUserInfo?.isNewUser ? 'Bienvenido' : 'Bienvenido'} secondaryText={`${user?.metadata?.name} ${gender}` + ` ${device} ${role}` || ''} secondaryTextMain={true} />
+									<TitleScreen icon={''} exterStyles={{ wrapper: { marginBottom: 30 }, primaryText: { fontSize: 36 }, secondaryText: { fontSize: 36 } }} primaryText={user?.additionalUserInfo?.isNewUser ? 'Bienvenido' : 'Bienvenido'} secondaryText={debug ? `${user?.metadata?.name} ${gender}` + ` ${device} ${role}` : `${user?.metadata?.name} ${gender}`} secondaryTextMain={true} />
 									<EmailVerify user={user || {}} />
 									{
 										{
