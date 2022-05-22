09c13577ad03b9c773259b39c3f6a722cd7b03c1
Pepe Fernández <jfernandezmarin0@uoc.edu>
2022-05-22 12:50:56 +0200

    Update version 1.0.4 & versionCode 5

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
 		name: 'Expert Garden',
 		slug: 'expert-garden',
 		description: 'An app to request gardening services',
-		version: '1.0.3',
+		version: '1.0.4',
 		githubUrl: 'https://github.com/PEP-UOC/expert-garden',
 		orientation: 'portrait',
 		backgroundColor: '#ffffff',
@@ -40,7 +40,7 @@ export default {
 		},
 		assetBundlePatterns: ['**/*'],
 		android: {
-			versionCode: 4,
+			versionCode: 5,
 			package: `com.expertgarden.${process.env.APP_VARIANT || 'expertgarden'}`,
 			googleServicesFile: './google-services.json',
 			adaptiveIcon: {
